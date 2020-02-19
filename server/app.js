6fc0f8d6744747837c86ba202066eccab02b4a9e
kemenykristof <kemeny.kristof24@gmail.com>
2020-02-19 15:50:07 +0100

    book and author model added, mongoose connection established

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -16,3 +17,11 @@ app.use(
 app.listen(4000, () => {
   console.log("now listening for requests on port 4000");
 });
+
+mongoose
+  .connect(
+    "mongodb+srv://kemenykristof:graphql123@cluster0-mvowg.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch(err => console.log("Error: ", err.message));
