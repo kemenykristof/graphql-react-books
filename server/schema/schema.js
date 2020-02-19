b340f5cfb01df469285dffafbede2c8e97485349
kemenykristof <kemeny.kristof24@gmail.com>
2020-02-19 15:16:03 +0100

    added new root queries(authors, books)

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -6,18 +6,19 @@ const {
   GraphQLString,
   GraphQLSchema,
   GraphQLID,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLList
 } = graphql;
 
 // dummy data
 const books = [
-  {name: 'Name of the Wind', genre: 'Fantasy', id: '1', authorId: '1'},
-  {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
-  {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
-  {name: 'The Hero of Ages', genre: 'Fantasy', id: '4', authorId: '2'},
-  {name: 'The Colour of Magic', genre: 'Fantasy', id: '5', authorId: '3'},
-  {name: 'The Light Fantastic', genre: 'Fantasy', id: '6', authorId: '3'}
-]
+  { name: "Name of the Wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The Final Empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The Long Earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+  { name: "The Hero of Ages", genre: "Fantasy", id: "4", authorId: "2" },
+  { name: "The Colour of Magic", genre: "Fantasy", id: "5", authorId: "3" },
+  { name: "The Light Fantastic", genre: "Fantasy", id: "6", authorId: "3" }
+];
 const authors = [
   { name: "Patrick Rothfuss", age: 44, id: "1" },
   { name: "Brandon Sanderson", age: 42, id: "2" },
@@ -65,6 +66,18 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return _.find(authors, { id: args.id });
+      }
+    },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve(parent, args) {
+        return books;
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args) {
+        return authors;
       }
     }
   }
